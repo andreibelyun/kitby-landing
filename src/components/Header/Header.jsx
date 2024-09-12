f814d8f360c70e69074e0d07761ae6fd6dcdb355
Andrey Belyun <andreibelyun@gmail.com>
2024-09-12 18:35:30 +0300

    feat: create header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,38 @@
 import st from './Header.module.scss'
+import Logo from '../Logo/Logo'
+import IconMail from '@/assets/svg/IconMail'
+import Button from '../Button/Button'
+import { CONTACT_EMAIL, PHONE_NUMBER, READABLE_PHONE_NUMBER } from '@/constants/contacts'
 
 const Header = () => {
-  return <header className={st.header}>Header</header>
+  return (
+    <header className={st.container}>
+      <Logo />
+
+      <h4 className={st.slogan}>
+        ваш надежный партнёр
+        <br />в поставках из Китая&nbsp;
+        <span className={st.flag}>🇨🇳</span>
+      </h4>
+
+      <div className={st.contacts}>
+        <a href={`mailto:${CONTACT_EMAIL}`} className={st.email}>
+          <IconMail />
+          <span>{CONTACT_EMAIL}</span>
+        </a>
+
+        <a href={`tel:${PHONE_NUMBER}`} className={st.phone}>
+          {READABLE_PHONE_NUMBER}
+        </a>
+      </div>
+
+      <Button className={st.btn} size='small' style={{ width: '150px' }}>
+        Оставить
+        <br />
+        заявку
+      </Button>
+    </header>
+  )
 }
 
 export default Header
