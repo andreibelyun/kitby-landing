fce971b7a11bd07b5ba056e6de3f47eb7dac570e
Andrey Belyun <andreibelyun@gmail.com>
2024-09-14 21:21:40 +0300

    feat: add swiper

---
+++ src/assets/svg/IconArrowRight.jsx
@@ -0,0 +1,9 @@
+const IconArrowRight = () => {
+  return (
+    <svg width='23' height='38' viewBox='0 0 23 38' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path d='M3.11243 35.2249L19.2249 19.1124L3.11243 3' stroke='white' strokeWidth='5' strokeLinecap='round' />
+    </svg>
+  )
+}
+
+export default IconArrowRight
---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,97 @@
+'use client'
+
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import st from './Carousel.module.scss'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Autoplay, Pagination } from 'swiper/modules'
+import IconArrowLeft from '@/assets/svg/IconArrowLeft'
+import IconArrowRight from '@/assets/svg/IconArrowRight'
+
+const DEFAULT_BREAKPOINTS = {
+  1400: {
+    spaceBetween: 46,
+    slidesPerView: 3.7
+  },
+
+  1200: {
+    spaceBetween: 20,
+    slidesPerView: 3
+  },
+
+  600: {
+    spaceBetween: 25,
+    slidesPerView: 2
+  },
+
+  500: {
+    spaceBetween: 0,
+    slidesPerView: 1
+  }
+}
+
+const PAGINATION = {
+  clickable: true
+}
+
+const Carousel = ({
+  id,
+  breakpoints = DEFAULT_BREAKPOINTS,
+  withNavigation = true,
+  withPagination = false,
+  withAutoplay = true,
+  slides,
+  className = '',
+  ...props
+}) => {
+  const navigationProps = {
+    prevEl: `#${id}-prev`,
+    nextEl: `#${id}-next`
+  }
+
+  return (
+    <div className={`${st.container} ${className}`}>
+      <Swiper
+        breakpoints={breakpoints}
+        modules={[Navigation, Autoplay, Pagination]}
+        autoplay={withAutoplay ? { delay: 2000 } : false}
+        pagination={withPagination ? PAGINATION : false}
+        navigation={withNavigation ? navigationProps : false}
+        style={{ paddingBottom: withPagination ? '45px' : '0' }}
+        {...props}
+      >
+        {slides.map((slide, i) => (
+          <SwiperSlide key={`history-slide-${i}`}>{slide}</SwiperSlide>
+        ))}
+      </Swiper>
+
+      {withNavigation && (
+        <div className={st.nav}>
+          <NavigationBtn id={`${id}-prev`} to='prev' />
+          <NavigationBtn id={`${id}-next`} to='next' />
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Carousel
+
+const NavigationBtn = ({ id, to, ...props }) => {
+  const cn = `${to === 'prev' ? st.prevBtn : st.nextBtn}`
+
+  return (
+    <div className={cn}>
+      <button
+        {...props}
+        id={id}
+        type='button'
+        className={st.navBtn}
+        aria-label={to === 'prev' ? 'Предыдущий слайд' : 'Следующий слайд'}
+      >
+        {to === 'prev' ? <IconArrowLeft /> : <IconArrowRight />}
+      </button>
+    </div>
+  )
+}
