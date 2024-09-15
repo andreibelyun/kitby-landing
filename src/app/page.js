c3d2af890425e0fed3100f14d60ce5e661473516
Andrey Belyun <andreibelyun@gmail.com>
2024-09-15 10:53:08 +0300

    fix: add team section to page

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import { ModalContextProvider } from '@/context/ModalContext'
 import Cover from '@/views/Cover/Cover'
 import WhyUs from '@/views/WhyUs/WhyUs'
 import Services from '@/views/Services/Services'
+import Team from '@/views/Team/Team'
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Cover />
       <WhyUs />
       <Services />
+      <Team />
     </ModalContextProvider>
   )
 }
