c004635b669a08f80fc2f41424f0c0c09f83d590
Andrey Belyun <andreibelyun@gmail.com>
2024-09-16 15:30:17 +0300

    feat: update input

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,8 +1,21 @@
 import st from './Input.module.scss'
 import clsx from 'clsx'
 
-const Input = ({ className, ...props }) => {
-  return <input className={clsx(st.input, className)} {...props} />
+const Component = ({ type, ...props }) => (type === 'textarea' ? <textarea {...props} /> : <input {...props} />)
+
+const Input = ({ type = 'input', id = '1', isError = false, className, labelText, ...props }) => {
+  return (
+    <label htmlFor={id} className={clsx(st.inputContainer, className)}>
+      {labelText}
+      <Component
+        {...props}
+        id={id}
+        type={type}
+        autoComplete='off'
+        className={clsx(st.input, { [st.error]: isError })}
+      />
+    </label>
+  )
 }
 
 export default Input
