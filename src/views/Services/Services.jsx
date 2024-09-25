c25027e1cec1bd6a647f8597b8b74010e2b3b3f2
Andrey Belyun <andreibelyun@gmail.com>
2024-09-25 16:30:40 +0300

    feat: make services section responsive

--- src/views/Services/Services.jsx
+++ src/views/Services/Services.jsx
@@ -1,19 +1,49 @@
 import st from './Services.module.scss'
 import clsx from 'clsx'
-import Carousel from '@/components/Carousel/Carousel'
 import { SERVICES_LIST } from './servicesList'
-import ButtonsGroup from '@/components/ButtonsGroup/ButtonsGroup'
 import ChFlag from '@/components/ChFlag/ChFlag'
+import Carousel from '@/components/Carousel/Carousel'
+import ButtonsGroup from '@/components/ButtonsGroup/ButtonsGroup'
 
 const ADDITIONAL = [
   'Проверка качества товара в Китае.',
   'Услуги по упаковке и маркировке.',
   <>
-    Организация поездок в Китай&nbsp;
+    Организация поездок в Китай &nbsp;
     <ChFlag />
   </>,
   'Изготовление товара по индивидуальному заказу клиента.'
 ]
+
+const BREAKPOINTS = {
+  1360: {
+    spaceBetween: 35,
+    slidesPerView: 3.7
+  },
+
+  1260: {
+    spaceBetween: 15,
+    slidesPerView: 3.4
+  },
+
+  1060: {
+    spaceBetween: 15,
+    slidesPerView: 2.8
+  },
+
+  960: {
+    spaceBetween: 15,
+    slidesPerView: 2.4
+  },
+
+  840: {
+    slidesPerView: 2,
+    spaceBetween: 10
+  },
+  500: {
+    slidesPerView: 1
+  }
+}
 
 const Services = () => {
   return (
@@ -24,6 +54,7 @@ const Services = () => {
         loop
         withPagination
         id='services-slider'
+        breakpoints={BREAKPOINTS}
         slides={SERVICES_LIST.map(info => (
           <ServiceSlide key={info.text} {...info} />
         ))}
@@ -53,7 +84,7 @@ const ServiceSlide = ({ bgImg, text, textSize = 'S' }) => (
     <div className={st.slideInner}>
       <p
         className={clsx(st.slideText, {
-          [st.slideTextS]: textSize === 'M',
+          [st.slideTextS]: textSize === 'S',
           [st.slideTextM]: textSize === 'M',
           [st.slideTextL]: textSize === 'L'
         })}
