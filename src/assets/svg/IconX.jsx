822fdaa9112594c57a0d3e7fafb02a5a6a5eef9a
Andrey Belyun <andreibelyun@gmail.com>
2024-10-07 15:31:00 +0300

    feat: add close button icon

--- src/assets/svg/IconX.jsx
+++ src/assets/svg/IconX.jsx
@@ -1,8 +1,16 @@
-const IconX = () => {
+const IconX = props => {
   return (
-    <svg width='40' height='40' viewBox='0 0 80 80' fill='none' xmlns='http://www.w3.org/2000/svg'>
-      <path d='M20.2446 20.5022L59.5062 59.7638' stroke='white' strokeWidth='5' />
-      <path d='M59.5063 20.5022L20.2447 59.7638' stroke='white' strokeWidth='5' />
+    <svg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
+      <rect x='2.98364' width='36.9032' height='4.19355' rx='1' transform='rotate(45 2.98364 0)' fill='white' />
+      <rect
+        x='29.0782'
+        y='2.96533'
+        width='36.9032'
+        height='4.19355'
+        rx='1'
+        transform='rotate(135 29.0782 2.96533)'
+        fill='white'
+      />
     </svg>
   )
 }
